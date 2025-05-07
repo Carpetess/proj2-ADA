32e680bb408b9bfb6b417fa3e231c1d84e9f353f
Carpetess <andre.filipe19062003@gmail.com>
2025-05-07 15:38:53 +0100

    Main parsing done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] firstLine = br.readLine().split(" ");
+        int locations = Integer.parseInt(firstLine[0]);
+        int roads = Integer.parseInt(firstLine[1]);
+
+        Edge[] edges = new Edge[roads];
+
+        for (int i = 0; i < roads; i++) {
+            String[] line = br.readLine().split(" ");
+            int first = Integer.parseInt(line[0]);
+            int second = Integer.parseInt(line[1]);
+            int hardness = Integer.parseInt(line[2]);
+            edges[i] = new Edge(first, second, hardness);
+        }
+
+        int numberOfOperations = Integer.parseInt(br.readLine());
+
+        int[][] operations = new int[numberOfOperations][2];
+        for (int i = 0; i < numberOfOperations; i++) {
+            String[] line = br.readLine().split(" ");
+            operations [i][0] = Integer.parseInt(line[0]);
+            operations[i][1] = Integer.parseInt(line[1]);
+        }
+
         }
     }
-}

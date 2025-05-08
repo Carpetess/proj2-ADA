fc4ebefe0e768f39e4df6e2b241b751b95ce8c47
Guigs13 <g.fialho@campus.fct.unl.pt>
2025-05-08 21:46:47 +0100

    commit

--- src/Solver.java
+++ src/Solver.java
@@ -45,4 +45,51 @@ public class Solver {
         edges.sort(byHardness);
         return edges;
     }
+
+
+    private Edge[] mstPrim( List<Edge> graph ) {
+        Edge[] mst = new Edge[numOfLocations - 1];
+        int mstSize = 0;
+        boolean[] selected = new boolean[numOfLocations];
+        int[] cost = new int[numOfLocations];
+        Edge[] via = new Edge[numOfLocations];
+        PriorityQueue<Edge> connected= new PriorityQueue<>(numOfLocations,Comparator.comparingInt(Edge::hardness));
+
+        for(int i= 0 ; i < numOfLocations ; i++) {
+            selected[i] = false;
+            cost[i] = -1;
+        }
+        Edge origin = graph.getFirst();
+        cost[origin.first()] = 0;
+        connected.add(origin);
+
+        do {
+            Edge edge = connected.poll();
+            selected[edge.first()] = true;
+            if ( node != origin )
+                mst[ mstSize++ ] = via[node];
+            exploreNode(graph, node, selected, cost, via, connected);
+        }
+        while ( !connected.isEmpty() );
+        return mst;
+    }
+
+    private void exploreNode( UndiGraph<L> graph, Node source,
+                      boolean[] selected, L[] cost, Edge<L>[] via,
+                      AdaptMinPriQueue<L, Node> connected ) {
+        for every Edge<L> e in graph.incidentEdges(source) {
+            Node node = e.oppositeNode(source);
+            if ( !selected[node] && e.label() < cost[node] ) {
+                boolean nodeIsInQueue = cost[node] < +∞;
+                cost[node] = e.label();
+                via[node] = e;
+                if ( nodeIsInQueue )
+                    connected.decreaseKey(node, cost[node]);
+                else
+                    connected.insert(cost[node], node);
+            }
+        }
+    }
+
+
 }
